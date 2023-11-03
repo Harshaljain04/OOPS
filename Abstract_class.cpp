5784ca0f44e525066e016d2cadef0fddf7964f00
Harshaljain04 <907920604+Harshaljain04@users.noreply.github.com>
2023-11-03 10:39:45 +0530

    03-11-23

---
+++ Abstract_class.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class super1{
+    public:
+    virtual void disp()=0;
+    int v1;
+};
+class sub2:public super1{
+    public:
+    void disp(){
+        cout<<"Access base class -super1- disp() in derived class sub1"<<endl;
+    }
+};
+int main()
+{
+    super1 *s1obj;
+    sub2 obj2;
+    s1obj=&obj2;
+    s1obj->disp();
+    return 0;
+}
