cd85fc38f6d1b699c6c580a3af23d25b647b6af0
Harshal Jain <harshaljn04@gmail.com>
2023-11-21 10:40:03 +0530

    21/11/23

--- Function_OverRiding.cpp
+++ Function_OverRiding.cpp
@@ -14,9 +14,21 @@ class derived:public Base{
         cout<<"Display of derived class";
     }
 };
+
+class extra:public derived, public Base{
+    public:
+    void display()
+    {
+        cout<<"Display of extra class";
+    }
+};
 int main()
 {
     Base B;
     B.display();  
+    Derived D;
+    D.display();
+    Extra C;
+    C.display();
     return 0;
 }
